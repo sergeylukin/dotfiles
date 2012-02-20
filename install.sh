86b40454e11ffc027ad9abb51510f9c5baf04eb0
Sergey Lukin <contact@sergeylukin.com>
2012-02-20 06:41:29 +0000

    Upgraded installation shell script CHANGELOG: 	- don't copy dotfiles for stuff that is not in use by currently logged in user 	- don't activate OH-MY-ZSH configuration file if OH-MY-ZSH is not installed 	- fixed OH-MY-ZSH copying operation 	- remove BACKUP dir if it exists before backing up

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 ############################
 # install.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+# This script creates symlinks from the home directory to dotfiles in ~/bin/dotfiles
+# It doesn't do any installations, just copies the configuration files for
+# ZSH, OH-MY-ZSH, VIM, GIT, ROXTERM and only if they are used.
+# Old installation files are backed up in ~/bin/dotfiles_old
 ############################
 
 ########## Variables
@@ -12,47 +15,75 @@ olddir=~/bin/dotfiles_old             # old dotfiles backup directory
 ##########
 
 echo "Creating $olddir for backup of any existing dotfiles in ~"
+# First - remove if it exists
+if [[ -d $olddir ]]
+then
+	rm -fr $olddir
+fi
+# Now - create fresh one
 mkdir -p $olddir
 echo "...done"
 
 # INSTALL ZSH DOTFILES
-echo "Backing up old ZSH files"
-mv ~/.zshrc $olddir/
-echo "...done"
-echo "Creating symlink to new ZSH files"
-ln -s $dir/zsh/zshrc ~/.zshrc
-echo "...done"
+if [[ -f ~/.zshrc ]]
+then
+	echo "Backing up old ZSH files"
+	mv ~/.zshrc $olddir/
+	echo "...done"
+	echo "Creating symlink to new ZSH files"
+	touch ~/.zshrc
+	echo ". $dir/zsh/zshrc" >> ~/.zshrc
+	echo "...done"
+fi
 
 # INSTALL OH-MY-ZSH
-echo "Backing up old OH-MY-ZSH files"
-cp -pR ~/.oh-my-zsh $olddir/
-echo "...done"
-echo "Overwriting old OH-MY-ZSH files with new files"
-cp -pr $dir/oh-my-zsh/ ~/.oh-my-zsh/
-echo "...done"
+if [ -f ~/.zshrc ] && [ -d ~/.oh-my-zsh ] # install oh-my-zsh files only if user already uses it and has ZSH installed
+then
+	echo "Backing up old OH-MY-ZSH files"
+	cp -pR ~/.oh-my-zsh $olddir/
+	echo "...done"
+	echo "Overwriting old OH-MY-ZSH files with new files"
+	cp -pr $dir/oh-my-zsh/. ~/.oh-my-zsh
+	echo "...done"
+	echo "Adding OH-MY-ZSH configuration file link to ~/.zshrc"
+	# because we don't want to change anything like that in repo
+	# we do it in ~/.zshrc
+	# $dir/zsh/zshrc is still in active use and all changes in it will take place
+	echo ". $dir/zsh/includes/oh-my-zsh" >> ~/.zshrc
+	echo "...done"
+fi
 
 # INSTALL VIM DOTFILES
-echo "Backing up VIM files"
-mv ~/.vim $olddir/
-mv ~/.vimrc $olddir/
-echo "...done"
-echo "Creating symlink to new VIM files"
-ln -s $dir/vim ~/.vim
-ln -s $dir/vim/vimrc ~/.vimrc
-echo "...done"
+if [[ -d ~/.vim ]]
+then
+	echo "Backing up VIM files"
+	mv ~/.vim $olddir/
+	mv ~/.vimrc $olddir/
+	echo "...done"
+	echo "Creating symlink to new VIM files"
+	ln -s $dir/vim ~/.vim
+	ln -s $dir/vim/vimrc ~/.vimrc
+	echo "...done"
+fi
 
 # INSTALL GIT DOTFILES
-echo "Backing up GIT --global config file"
-mv ~/.gitconfig $olddir/
-echo "...done"
-echo "Creating symlink to new GIT --global config file"
-ln -s $dir/gitconfig ~/.gitconfig
-echo "...done"
+if [[ -f ~/.gitconfig ]]
+then
+	echo "Backing up GIT --global config file"
+	mv ~/.gitconfig $olddir/
+	echo "...done"
+	echo "Creating symlink to new GIT --global config file"
+	ln -s $dir/gitconfig ~/.gitconfig
+	echo "...done"
+fi
 
 # INSTALL ROXTERM DOTFILES
-echo "Backing up ROXTERM files"
-mv ~/.config/roxterm.sourceforge.net $olddir/
-echo "...done"
-echo "Creating symlink to new ROXTERM files"
-ln -s $dir/roxterm ~/.config/roxterm.sourceforge.net
-echo "...done"
+if [[ -d ~/.config/roxterm.sourceforge.net ]]
+then
+	echo "Backing up ROXTERM files"
+	mv ~/.config/roxterm.sourceforge.net $olddir/
+	echo "...done"
+	echo "Creating symlink to new ROXTERM files"
+	ln -s $dir/roxterm ~/.config/roxterm.sourceforge.net
+	echo "...done"
+fi
